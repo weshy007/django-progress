bcf1e79f5fb5300ba53e1e791f8abaefa4a41d10
weshy <josephwaweru96@gmail.com>
2022-05-07 23:33:18 +0300

    db config

--- tribune/settings.py
+++ tribune/settings.py
@@ -85,8 +85,10 @@ WSGI_APPLICATION = 'tribune.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tribune',
+        'USER': 'weshy',
+    'PASSWORD':'weshy',
     }
 }
 
